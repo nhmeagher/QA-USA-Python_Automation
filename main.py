ed1895dc622c6d1e06c7da7c8ce0f0383e0ab6a1
nhmeagher <nhmeagher@gmail.com>
2025-07-18 15:40:08 -0500

    Final Update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import data
 import helpers
 
-
 class TestUrbanRoutes:
     @classmethod
     def setup_class(cls):
@@ -15,43 +14,36 @@ class TestUrbanRoutes:
         print("function created for set route")
         pass
 
-
     def test_select_plan(self):
         # Add in S8
         print("function created for test_select_plan")
         pass
-
 
     def test_fill_phone_number(self):
         # Add in S8
         print("function created for test_fill_phone_number")
         pass
 
-
     def test_fill_card(self):
         # Add in S8
         print("function created for test_fill_card")
         pass
-
 
     def test_comment_for_driver(self):
         # Add in S8
         print("function created for test_comment_for_driver")
         pass
 
-
     def test_order_blanket_and_handkerchiefs(self):
         # Add in S8
         print("function created for test_order_blanket_and_handkerchiefs")
         pass
-
 
     def test_order_2_ice_creams(self):
         for i in range(2):  # This loops 2 times
             # Add in S8
             print("function created for test_order_2_ice_creams")
         pass
-
 
     def test_car_search_model_appears(self):
         # Add in S8
